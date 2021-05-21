8c77da32765cb8526206a4f5a5d2cceeba8b1511
unknown <nafuza1299@gmail.com>
2021-05-21 15:51:55 +0700

    input data

--- test-tool.py
+++ test-tool.py
@@ -1,5 +1,11 @@
 import sys
+import json
 import utility
 url = "https://csit314-testing-tool.herokuapp.com"
 arguement = sys.argv
-print(arguement[1])
+
+
+with open(arguement[1]) as f:
+    data = json.load(f)
+    for i in data:
+        utility.get_request(i['url'])
--- utility.py
+++ utility.py
@@ -2,19 +2,14 @@ import pytest
 import requests
 import json
 
-#get request
-url = "https://csit314-testing-tool.herokuapp.com/contacts"
-res = requests.get(url)
-get_Content = json.loads(res.content)['contacts']
-print(get_Content)
+
+def get_request(url):
+    #get request
+    res = requests.get(url)
+    get_Content = json.loads(res.content)['contacts']
+    print(get_Content)
+
 #post request
-data = {
-  "contact":
-    {
-        "id": "tseter",
-        "first": "Test",
-        "last": "Test2"
-    }
-}
-x = requests.post(url, data = data)
-print(x)
+def post_request(url, data):
+    x = requests.post(url, data = data)
+    print(x)
