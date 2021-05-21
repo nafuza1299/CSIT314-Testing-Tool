f9c2b181afa451a9243c3445c0b16ffb150f606c
unknown <nafuza1299@gmail.com>
2021-05-21 16:41:17 +0700

    headers and post

--- test-tool.py
+++ test-tool.py
@@ -8,4 +8,11 @@ arguement = sys.argv
 with open(arguement[1]) as f:
     data = json.load(f)
     for i in data:
-        utility.get_request(i['url'])
+       if(i['type'].lower() == 'post'):
+            utility.post_request(i['url'], i['body'], i['header'])
+            check_url = i['check']['url'].replace('{{id}}', i['body']['contact']['id'])
+            utility.get_request(check_url, i['header'])
+       elif(i['type'].lower() == 'get'):
+           utility.get_request(i['url'], i['header'])
+
+
--- utility.py
+++ utility.py
@@ -3,13 +3,19 @@ import requests
 import json
 
 
-def get_request(url):
+def get_request(url, headers):
     #get request
-    res = requests.get(url)
-    get_Content = json.loads(res.content)['contacts']
-    print(get_Content)
+    res = requests.get(url, headers = headers)
+    get_Content = json.loads(res.content)
+    print("GET REQUEST: ", res)
+    print("GET CONTENT: ", get_Content)
 
 #post request
-def post_request(url, data):
-    x = requests.post(url, data = data)
-    print(x)
+def post_request(url, data, headers):
+    body = json.dumps(data)
+    res = requests.post(url, headers = headers ,  data = body)
+    get_Content = json.loads(res.content)
+    print("POST REQUEST: ", res)
+    print("POST CONTENT: ", get_Content)
+
+
