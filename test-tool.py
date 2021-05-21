2061d9805da2fd47d00ab7c7fe01d94fbfac8c10
unknown <nafuza1299@gmail.com>
2021-05-21 18:10:29 +0700

    content asssert put post

--- test-tool.py
+++ test-tool.py
@@ -9,13 +9,13 @@ with open(arguement[1]) as f:
     x = 1
     for i in data:
         print("Test", x)
-
         if(i['type'].lower() == 'post'):
             post_req = utility.post_request(i['url'], i['body'], i['header'])
             check_url = i['check']['url'].replace('{{id}}', i['body']['contact']['id'])
             get_req = utility.get_request(check_url, i['header'])
             utility.request_assert(post_req.status_code, i['check']['success_code'])
             utility.request_assert(get_req.status_code, 200)
+            utility.content_assert(i['body'], get_req)
 
         elif(i['type'].lower() == 'get'):
            get_req = utility.get_request(i['url'], i['header'])
@@ -30,6 +30,8 @@ with open(arguement[1]) as f:
             get_req = utility.get_request(i['check']['url'], i['header'])
             utility.request_assert(put_req.status_code, i['check']['success_code'])
             utility.request_assert(get_req.status_code, 200)
+            utility.content_assert(i['body'], get_req)
+
 
         x += 1
 
--- utility.py
+++ utility.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 import json
 
-# get request
+#get request
 def get_request(url, headers):
     res = requests.get(url, headers = headers)
     get_Content = json.loads(res.content)
@@ -19,13 +19,13 @@ def post_request(url, data, headers):
     print("POST CONTENT: ", get_Content)
     return res
 
-# delete request
+#delete request
 def delete_request(url, headers):
     res = requests.delete(url, headers = headers)
     print("DELETE REQUEST: ", res.status_code)
     return res
 
-# put request
+#put request
 def put_request(url, data, headers):
     body = json.dumps(data)
     res = requests.put(url, headers = headers, data = body)
@@ -38,3 +38,16 @@ def put_request(url, data, headers):
 def request_assert(res, code):
     assert int(res) == int(code)
 
+#assert content
+def content_assert(input, output):
+    #load output content
+    get_Content = json.loads(output.content)
+
+    #loop through the content body
+    for key in input:
+        output = get_Content[key]
+        input = input[key]
+        for i in input:
+            input_data = input[i]
+            output_data = output[i]
+            assert str(input_data) == str(output_data)
