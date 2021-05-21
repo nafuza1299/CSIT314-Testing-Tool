c492b036efb7c288c1b1245237b8449c9edca561
unknown <nafuza1299@gmail.com>
2021-05-21 17:09:51 +0700

    assertion

--- test-tool.py
+++ test-tool.py
@@ -6,14 +6,24 @@ arguement = sys.argv
 
 with open(arguement[1]) as f:
     data = json.load(f)
+    x = 1
     for i in data:
-       if(i['type'].lower() == 'post'):
-            utility.post_request(i['url'], i['body'], i['header'])
-            check_url = i['check']['url'].replace('{{id}}', i['body']['contact']['id'])
-            utility.get_request(check_url, i['header'])
-       elif(i['type'].lower() == 'get'):
-           utility.get_request(i['url'], i['header'])
-       elif (i['type'].lower() == 'delete'):
-           utility.delete_request(i['url'], i['header'])
+        print("Test", x)
 
+        if(i['type'].lower() == 'post'):
+            post_req = utility.post_request(i['url'], i['body'], i['header'])
+            check_url = i['check']['url'].replace('{{id}}', i['body']['contact']['id'])
+            get_req = utility.get_request(check_url, i['header'])
+            utility.request_assert(post_req.status_code, i['check']['success_code'])
+            utility.request_assert(get_req.status_code, 200)
+
+        elif(i['type'].lower() == 'get'):
+           get_req = utility.get_request(i['url'], i['header'])
+           utility.request_assert(get_req.status_code, i['check']['success_code'])
+
+        elif (i['type'].lower() == 'delete'):
+           del_req = utility.delete_request(i['url'], i['header'])
+           utility.request_assert(del_req.status_code, i['check']['success_code'])
+
+        x += 1
 
--- utility.py
+++ utility.py
@@ -2,25 +2,29 @@ import pytest
 import requests
 import json
 
-
 # get request
 def get_request(url, headers):
     res = requests.get(url, headers = headers)
     get_Content = json.loads(res.content)
-    print("GET REQUEST: ", res)
+    print("GET REQUEST: ", res.status_code)
     print("GET CONTENT: ", get_Content)
+    return res
 
 #post request
 def post_request(url, data, headers):
     body = json.dumps(data)
     res = requests.post(url, headers = headers ,  data = body)
     get_Content = json.loads(res.content)
-    print("POST REQUEST: ", res)
+    print("POST REQUEST: ", res.status_code)
     print("POST CONTENT: ", get_Content)
-
+    return res
 
 # delete request
 def delete_request(url, headers):
     res = requests.delete(url, headers = headers)
-    print("DELETE REQUEST: ", res)
+    print("DELETE REQUEST: ", res.status_code)
+    return res
 
+#assert code
+def request_assert(res, code):
+    assert int(res) == int(code)
