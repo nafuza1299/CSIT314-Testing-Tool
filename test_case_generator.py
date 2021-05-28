18a1fa39858452402c88a77a4e457bb3cf086ea6
unknown <nafuza1299@gmail.com>
2021-05-29 00:40:38 +0700

    assert case generator

---
+++ test_case_generator.py
@@ -0,0 +1,56 @@
+import case_generator
+import json
+
+#test result from delete generate dict using post request, contact and id
+def delete_generate():
+    data = {
+        "url": "https://csit314-testing-tool.herokuapp.com/contacts",
+        "type": "POST",
+        "header": {
+            "Content-Type": "application/json",
+            "Authorization": "ae34g1ce"
+        },
+        "body": {
+            "contact": {
+                "id": 999,
+                "first": "FFEeoeGynvucKklsPuwM",
+                "last": "fdTZqbsAuNFOymGMGFKC"
+            }
+        },
+        "check": {
+            "url": "https://csit314-testing-tool.herokuapp.com/contacts/821",
+            "success_code": "201"
+        }
+    }
+    res = case_generator.delete_generate(data, 'contact', 'id')
+    answer = {'url': 'https://csit314-testing-tool.herokuapp.com/contacts/999', 'type': 'DELETE', 'header': {'Authorization': 'ae34g1ce'}, 'check': {'url': 'https://csit314-testing-tool.herokuapp.com/contacts/999', 'success_code': '200'}}
+    assert json.dumps(res) == json.dumps(answer)
+    assert res == answer
+
+
+#test result from put generate dict using post request, contact and id
+def put_generate():
+    data = {
+        "url": "https://csit314-testing-tool.herokuapp.com/contacts",
+        "type": "POST",
+        "header": {
+            "Content-Type": "application/json",
+            "Authorization": "ae34g1ce"
+        },
+        "body": {
+            "contact": {
+                "id": 999,
+                "first": "FFEeoeGynvucKklsPuwM",
+                "last": "fdTZqbsAuNFOymGMGFKC"
+            }
+        },
+        "check": {
+            "url": "https://csit314-testing-tool.herokuapp.com/contacts/821",
+            "success_code": "201"
+        }
+    }
+    res = case_generator.put_generate(data, 'contact', 'id')
+    answer = {'url': 'https://csit314-testing-tool.herokuapp.com/contacts/999', 'type': 'PUT', 'header': {'Content-Type': 'application/json', 'Authorization': 'ae34g1ce'}, 'body': {'contact': {'first': 'FFEeoeGynvucKklsPuwM', 'last': 'fdTZqbsAuNFOymGMGFKC'}}, 'check': {'url': 'https://csit314-testing-tool.herokuapp.com/contacts/999', 'success_code': '200'}}
+    assert json.dumps(res) == json.dumps(answer)
+    assert res == answer
+put_generate()
