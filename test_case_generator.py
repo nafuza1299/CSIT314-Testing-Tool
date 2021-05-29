2dc38e0429193e57be2e8468a8802fc000a402c0
unknown <nafuza1299@gmail.com>
2021-05-30 00:43:22 +0700

    assert post_gen

--- test_case_generator.py
+++ test_case_generator.py
@@ -53,3 +53,28 @@ def test_put_generation():
     answer = {'url': 'https://csit314-testing-tool.herokuapp.com/contacts/999', 'type': 'PUT', 'header': {'Content-Type': 'application/json', 'Authorization': 'ae34g1ce'}, 'body': {'contact': {'first': 'FFEeoeGynvucKklsPuwM', 'last': 'fdTZqbsAuNFOymGMGFKC'}}, 'check': {'url': 'https://csit314-testing-tool.herokuapp.com/contacts/999', 'success_code': '200'}}
     assert json.dumps(res) == json.dumps(answer)
     assert res == answer
+    
+#assert dictionary key of post generation, as post generates random value
+def test_post_generation():
+    data = {
+        "url": "https://csit314-testing-tool.herokuapp.com/contacts",
+        "type": "POST",
+        "header": {
+            "Content-Type": "application/json",
+            "Authorization": "ae34g1ce"
+        },
+        "body": {
+            "contact": {
+                "id": "int",
+                "first": "string",
+                "last": "string"
+            }
+        },
+        "check": {
+            "url": "https://csit314-testing-tool.herokuapp.com/contacts/821",
+            "success_code": "201"
+        }
+    }
+    res = case_generator.post_generate(data, 'contact', 'id')
+    answer = {'url': 'https://csit314-testing-tool.herokuapp.com/contacts', 'type': 'POST', 'header': {'Content-Type': 'application/json', 'Authorization': 'ae34g1ce'}, 'body': {'contact': {'id': 8784, 'first': 'QwNUJiicGY WiqYOdOpYT', 'last': 'plbLkSpZJqBwqpnzVmtE'}}, 'check': {'url': 'https://csit314-testing-tool.herokuapp.com/contacts/8784', 'success_code': '200'}}
+    assert res.keys() == answer.keys()
